4648be0a3a9aea4d7b53ac9584c37e0b0b0f2ead
Andrew Arasmith <andara@Andrews-MacBook-Pro.local>
2022-12-14 18:16:29 +0100

    deleted code that has been moved elsewhere

--- update.py
+++ update.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import db
-import archive_logging
 import vids
-import psycopg2
 import pandas as pd
-import os
-import yt_dlp
-import sys
-import time
-import logging
+
 
 def main_function():
     #logger = archive_logging.create_kafka_logger(topic_name = 'archive_log')
@@ -31,48 +25,3 @@ def main_function():
 
 if __name__ == "__main__":
     main_function()
-
-#no_download_ids = pd.DataFrame(columns=["id_", "exception_type", "exception_message"])
-#downloaded_ids = pd.DataFrame(columns=["id_"])
-# if len(cf_db) > 0:
-#     #no_download_ids = pd.read_parquet(proj_path + "no_download_list.parquet")
-#     #downloaded_ids = pd.read_parquet(proj_path + "downloaded_list.parquet")
-    
-#     for i in list(range(0, len(cf_db), 1)):
-#         entry = cf_db.iloc[i]
-#         url = entry.url
-#         file_name = proj_path + "cf_vids/" + entry.id
-#         ydl_opts = {
-#             "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
-#             "outtmpl": f"{file_name}.mp4",
-#             "cookiefile": "yt_cookies.txt",
-#             "noplaylist": True
-#             }
-#         try:
-#             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-#                 ydl.download([url])
-#         except BaseException as ex:
-#             ex_type, ex_value = sys.exc_info()[0:2]
-#             #error = pd.DataFrame({"id_": [entry.id], "exception_type": [ex_type.__name__], "exception_message": [ex_value.msg]})
-#             #no_download_ids = pd.concat([no_download_ids, error])
-#             error = (entry.id, ex_type.__name__, ex_value.msg)
-#             cursor.execute("INSERT INTO errors VALUES (%s, %s, %s)", error)
-#         else:
-#             #complete = pd.DataFrame({"id_": [entry.id]})
-#             cursor.execute("INSERT INTO downloaded VALUES (%s)", entry.id)
-#             #downloaded_ids = pd.concat([downloaded_ids, complete])
-        
-    
-#     #no_download_ids.to_parquet(proj_path + "no_download_list.parquet")
-#     #downloaded_ids.to_parquet(proj_path + "downloaded_list.parquet")
-    
-#     old_place = str(place)
-#     new_place = str(max(cf_db.created_utc))
-    
-#     with open(proj_path + "place.txt", 'w') as f:
-#         f.write(new_place)
-    
-#     print(f"Log entry: Downloaded from {old_place} to {new_place} at {time.time()}")
-
-# else:
-#     print("Log entry: No entries to download: {time.time()}")
