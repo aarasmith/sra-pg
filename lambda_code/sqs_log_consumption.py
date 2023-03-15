ff28ed25f250f336fc256fff511ee2f3c07659ed
aarasmith <43914177+aarasmith@users.noreply.github.com>
2023-03-15 12:16:30 +0100

    Update sqs_log_consumption.py

--- lambda_code/sqs_log_consumption.py
+++ lambda_code/sqs_log_consumption.py
@@ -1,12 +1,13 @@
 import json
+import os
 import db
 
 def consume_logs(subreddit, messages):
     
     logs = [json.loads(message['body']) for message in messages]
     log_tuple_list = [tuple(x.values()) for x in logs]
-    
-    with db.get_connection(search_path=subreddit) as con:
+
+    with db.get_connection(search_path=subreddit, secret_id=os.environ['secret_id']) as con:
         cur = con.cursor()
         cur.executemany("INSERT INTO logs VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
                          log_tuple_list
